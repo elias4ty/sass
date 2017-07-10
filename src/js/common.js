2c9540fe933966cc062b0d71bf7598997a289b41
tangyang <tangyang@sogou-inc.com>
2017-07-10 08:44:10 +0800

    兼容

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,13 @@
 (function(){
-  var width = window.screen.width;
-  var html = document.getElementsByTagName('html')[0];
-  html.setAttribute('style','font-size:'+(width/10)+'px');
+  function fz(){
+    var width = window.screen.width;
+    var html = document.getElementsByTagName('html')[0];
+    html.setAttribute('style','font-size:'+(width/10)+'px');
+    console.log('123')
+  }
+
+  fz();
+
+  window.addEventListener('resize',fz);
+  window.addEventListener('orientation',fz);
 })()
